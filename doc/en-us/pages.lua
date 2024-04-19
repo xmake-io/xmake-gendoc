8e5d33682df958f59989b1ecc4c4ae8ae354e7e4
charles seizilles <charles.seizilles@iconeus.com>
2024-04-19 21:55:19 +0200

    enhance localization

--- doc/en-us/pages.lua
+++ doc/en-us/pages.lua
@@ -1,31 +1,35 @@
 {
-    {
-        title = "index",
-        pages = {
-            {
-                docdir = ".",
-                title = "index",
+    name = "English",
+    flag = "🇬🇧",
+    categories = {
+        {
+            title = "index",
+            pages = {
+                {
+                    docdir = ".",
+                    title = "index",
+                },
             },
         },
-    },
-    {
-        title = "API Manual (Description Scope)",
-        pages = {
-            {
-                docdir = "api/builtin_variables",
-                title = "Built-in variables",
-            },
-            {
-                docdir = "api/conditions",
-                title = "Conditions",
-            },
-            {
-                docdir = "api/builtin_modules/exceptions",
-                title = "Exceptions",
-            },
-            {
-                docdir = "api/builtin_modules/os",
-                title = "os",
+        {
+            title = "API Manual (Description Scope)",
+            pages = {
+                {
+                    docdir = "api/builtin_variables",
+                    title = "Built-in variables",
+                },
+                {
+                    docdir = "api/conditions",
+                    title = "Conditions",
+                },
+                {
+                    docdir = "api/builtin_modules/exceptions",
+                    title = "Exceptions",
+                },
+                {
+                    docdir = "api/builtin_modules/os",
+                    title = "os",
+                },
             },
         },
     },
